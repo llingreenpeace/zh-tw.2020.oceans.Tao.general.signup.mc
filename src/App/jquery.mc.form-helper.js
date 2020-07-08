2ed26d6590cf8fd4f51f433794712bd0227dd5ca
Up Chen <wallat@gmail.com>
2020-07-08 15:22:42 +0800

    Migrate to mc version.

---
+++ src/App/jquery.mc.form-helper.js
@@ -0,0 +1,242 @@
+const {$} = window
+
+$.validator.messages.required = "必填欄位";
+
+$.validator.addMethod('validate-name',
+	(value, element) => {
+		return new RegExp(/^[\u4e00-\u9fa5_a-zA-Z_ ]{1,40}$/i).test(value);
+	},
+	'姓氏格式不正確，請不要輸入數字或符號'
+);
+
+//override email with django email validator regex - fringe cases: "user@admin.state.in..us" or "name@website.a"
+$.validator.addMethod('email',
+	(value, element) => {
+		if (value) {
+			return /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/i.test(value);
+		}
+
+		return true
+	},
+	'Email 格式錯誤'
+);
+
+$.validator.addMethod( "taiwan-phone",
+	(value, element) => {
+		const phoneReg6 = new RegExp(/^(0|886|\+886)?(9\d{8})$/).test(value);
+		const phoneReg7 = new RegExp(/^(0|886|\+886){1}[2-8]-?\d{6,8}$/).test(value);
+
+		if (value) {
+			return (phoneReg6 || phoneReg7)
+		}
+		return true
+	},
+	"電話格式不正確，請只輸入數字 0912345678 和 02-23612351"
+)
+
+$.validator.addClassRules({ // connect it to a css class
+	"email": {email: true},
+	"taiwan-phone" : { "taiwan-phone" : true }
+});
+
+/**
+ * Collect all the input values inside the form
+ * @param  {DOM or string} form Native js Element (not in jquery form)
+ * @return {Object} {name:value, ...}
+ */
+export const fetchFormInputs = (form) => {
+	if (typeof form === 'string' || form instanceof String) {
+		form = document.querySelector(form)
+	}
+
+	let obj = {}
+	form.querySelectorAll("input").forEach(function (el, idx) {
+	  let v = null
+
+	  if (el.type==="checkbox") {
+	    v = el.checked
+	  } else {
+	    v = el.value
+	  }
+
+	  obj[el.name] = v
+	})
+	form.querySelectorAll("select").forEach(function (el, idx) {
+		obj[el.name] = el.options[el.selectedIndex].value
+	})
+
+	return obj
+}
+
+/**
+ * Retrieve the form POST URL
+ * @return {string} URL
+ */
+export const getPostURL = () => {
+	return document.querySelector("#mc-form").action
+}
+
+/**
+ * Retrieve the current number of supporters and signup targets
+ * @return {object} {numSignup:int, numSignupTarget:int}
+ */
+export const getNumSignupsAndTarget = () => {
+	var numSignupTarget = parseInt(document.querySelector('input[name="numSignupTarget"]').value, 10) || 0,
+		numResponses = parseInt(document.querySelector('input[name="numResponses"]').value, 10) || 0
+
+	return {
+		numSignupTarget,
+		numSignup: numResponses
+	}
+}
+
+/**
+ * Display the full loading screen
+ *
+ * Remember to add the following style to your page:
+ */
+/*
+
+#page-loading {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0,0,0,0.8);
+    z-index: 10001;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    opacity: 1;
+    transition: opacity 1s;
+}
+#page-loading.hide {
+    opacity: 0;
+    transition: opacity 1s;
+}
+
+#page-loading .lds-ellipsis {
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+}
+#page-loading .lds-ellipsis div {
+    position: absolute;
+    top: 33px;
+    width: 13px;
+    height: 13px;
+    border-radius: 50%;
+    background: #6c0;
+    animation-timing-function: cubic-bezier(0, 1, 1, 0);
+}
+#page-loading .lds-ellipsis div:nth-child(1) {
+    left: 8px;
+    animation: lds-ellipsis1 0.6s infinite;
+}
+#page-loading .lds-ellipsis div:nth-child(2) {
+    left: 8px;
+    animation: lds-ellipsis2 0.6s infinite;
+}
+#page-loading .lds-ellipsis div:nth-child(3) {
+    left: 32px;
+    animation: lds-ellipsis2 0.6s infinite;
+}
+#page-loading .lds-ellipsis div:nth-child(4) {
+    left: 56px;
+    animation: lds-ellipsis3 0.6s infinite;
+}
+@keyframes lds-ellipsis1 {
+    0% {transform: scale(0); }
+    100% {transform: scale(1); }
+}
+@keyframes lds-ellipsis3 {
+    0% {transform: scale(1); }
+    100% {transform: scale(0); }
+}
+@keyframes lds-ellipsis2 {
+    0% {transform: translate(0, 0); }
+    100% {transform: translate(24px, 0); }
+}
+ */
+export const showFullPageLoading = () => {
+	if ( !document.querySelector("#page-loading")) {
+		document.querySelector("body").insertAdjacentHTML('beforeend', `
+			<div id="page-loading" class="hide">
+			  <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+			</div>`);
+	}
+
+	setTimeout(() => { // to enable the transition
+		document.querySelector("#page-loading").classList.remove("hide")
+	}, 0)
+}
+
+/**
+ * Hide the full page loading
+ */
+export const hideFullPageLoading = () => {
+	document.querySelector("#page-loading").classList.add("hide")
+
+	setTimeout(() => {
+		document.querySelector("#page-loading").remove()
+	}, 1100)
+}
+
+/**
+ * Send the tracking event to the ga
+ * @param  {string} eventLabel The ga trakcing name, normally it will be the short campaign name. ex 2019-plastic_retailer
+ * @param  {[type]} eventValue Could be empty
+ * @return {[type]}            [description]
+ */
+export const sendPetitionTracking = (eventLabel, eventValue) => {
+	window.dataLayer = window.dataLayer || [];
+
+	window.dataLayer.push({
+	    'event': 'gaEvent',
+	    'eventCategory': 'petitions',
+	    'eventAction': 'signup',
+	    'eventLabel': eventLabel,
+	    'eventValue' : eventValue
+	});
+
+	window.dataLayer.push({
+	    'event': 'fbqEvent',
+	    'contentName': eventLabel,
+	    'contentCategory': 'Petition Signup'
+	});
+}
+
+/**
+ * We provide two helper class to quickly switch the block show/hide
+ * in the dd page: `is-hidden-at-dd-page-only`, `is-shown-at-dd-page-only`
+ *
+ * This function use the current url to distinguish the dd page.
+ * The dd page should have url like this: xxx?utm_source=dd
+ */
+(() => {
+	if (window.location.href.indexOf("utm_source=dd")>=0) {
+		let style = document.createElement('style');
+		style.innerHTML =
+			`.is-hidden-at-dd-page-only {
+				display: none !important;
+			}
+			.is-shown-at-dd-page-only {
+				display: block !important;
+			}`
+		;
+		document.head.appendChild(style);
+	} else { // not in the dd page
+		let style = document.createElement('style');
+		style.innerHTML =
+			`
+			.is-shown-at-dd-page-only {
+				display: none !important;
+			}`
+		;
+		document.head.appendChild(style);
+	}
+})()
--- src/App/main.js
+++ src/App/main.js
@@ -1,26 +1,20 @@
 import './main.scss'
+import {
+	fetchFormInputs,
+	getPostURL,
+	getNumSignupsAndTarget,
+	showFullPageLoading,
+	hideFullPageLoading,
+	sendPetitionTracking } from './jquery.mc.form-helper.js'
 
 const {$, history, AOS} = window
-
-$(document).ready(function () {
-	// show the petition succ info
-	if (window.location.pathname.indexOf('/petition/2') >= 0 && $(window).width() <= 800) {
-		// This prevents the page from scrolling down to where it was previously.
-		if ('scrollRestoration' in history) {
-			history.scrollRestoration = 'manual';
-		}
-
-		let enc = document.querySelector(".en__component")
-		let top = enc.getBoundingClientRect().top+window.pageYOffset
-		window.scrollTo(0, top);
-	}
-});
 
 var input = $('.typing__input').val();
 var counter = 0;
 var freq = 100;
 var timer = null;
 
+// display the typing effect
 var typing = () => {
 	let current_input = input.substr(0, counter);
 	let last = current_input.substr(current_input.length - 1);
@@ -37,10 +31,7 @@ var typing = () => {
 	}
 }
 
-
-
 $(document).ready(function() {
-
 	AOS.init({
 		easing: 'ease-out-back',
 		duration: 1000,
@@ -75,73 +66,206 @@ $(document).ready(function() {
 	$(".downward-div").click(function() {
 		$.fn.fullpage.moveSectionDown();
 	});
-
-	//for EN
-	$('.en__field--text').each(function() {
-		var input = $(this).find('input');
-		var label = $(this).find('label');
-		var div = $(this).find('.en__field__element');
-
-		var labelText = label.text();
-		label.hide();
-
-		if (labelText === '電郵地址' || labelText === '電子郵件') {
-			input.attr('placeholder', '電子郵件');
-			input.attr('id', 'email');
-			input.clone().attr('type', 'email').insertAfter(input).prev().remove();
-		} else if (labelText == '中文姓名') {
-			input.attr('placeholder', '中文姓名');
-			input.attr('id', 'full_name');
-			if (input_hide) { div.hide(); }
-		} else if (['姓氏', '名字'].indexOf(labelText) > -1) {
-			input.attr('placeholder', labelText);
-			// input.attr('id', 'full_name');
-			if (input_hide) { div.hide(); }
-		} else if (labelText == '聯絡電話') {
-			input.attr('placeholder', '聯絡電話');
-			input.attr('id', 'phone_number');
-			input.clone().attr('type', 'tel').insertAfter(input).prev().remove();
-			if (input_hide) { div.hide(); }
-		}
-	});
-
-	$('.en__field--select').find('label').hide();
-	if (input_hide) { $('.en__field--select').find('div').hide(); }
-	// $('.en__field__input--select').append($('<option>', {
-	// 	value: '',
-	// 	text: '出生年份'
-	// }));
-	for (var i = new Date().getFullYear(); i > 1900; i--) {
-		$('.en__field__input--select').append($('<option>', {
-			value: '01/01/' + i.toString(),
-			text: i
-		}));
-	}
-
-	$('.en__field--email-ok-taiwan').find('label').first().hide();
-	$('.en__field--email-ok-taiwan').insertAfter($('.en__submit'));
-	if (input_hide) { $('.en__field--email-ok-taiwan').hide(); }
-	$('.en__component--contactblock').insertBefore($('#email'));
-	$('.en__field__label--item').html($('.en__field__label--item').text().replace('隱私保護政策', '<a href="http://www.greenpeace.org/taiwan/aboutus/privacy/">隱私保護政策</a>'));
-
-	$('#email').focus(function() {
-		$('.en__field__element, .en__field--checkbox').slideDown(400);
-	});
-
-	$('.en__submit').click(function() {
-		$('.en__field__element, .en__field--checkbox').slideDown(400);
-	});
-
-	$('.cta__btn').click(function() {
-		$('.en__field__element, .en__field--checkbox').slideDown(400);
-		$('html, body').animate({ scrollTop: $('#email').offset().top }, 'slow');
-		$('#email').focus();
-	});
 });
 
-// swap images
+// swap images to make it looks an animation
 $(() => {
 	setInterval(() => {
 		$(".before-after img").toggleClass("active");
 	}, 1000)
+})
+
+
+// handle form inilialize
+$(() => {
+	// Generate the year selections
+	let nowYear = new Date().getFullYear()
+	for (var i = nowYear; i > nowYear-100; i--) {
+		$('.en__field__input--select').append($('<option>', {
+			value: i.toString()+"-01-01",
+			text: i
+		}));
+	}
+
+	// expand the hidden fields while focus to email inputs
+	$('#email').focus(function() {
+		$('.slide-down-panel').slideDown(400);
+	});
+
+	$('.en__submit').click(function() {
+		$('.slide-down-panel').slideDown(400);
+	});
+
+	$('.cta__btn').click(function() {
+		$('.slide-down-panel').slideDown(400);
+		$('html, body').animate({ scrollTop: $('#email').offset().top }, 'slow');
+		$('#email').focus();
+	});
+
+	// initialize the validations
+
+
+	$("form.en__component").validate({
+		errorPlacement: function(error, element) {
+			console.log(error)
+			// element.parents("div.form-field:first").after( error );
+			element.after( error );
+		},
+		submitHandler: function(form) {
+			showFullPageLoading()
+
+			try {
+				let defaults = fetchFormInputs("#mc-form")
+				let userInputs = fetchFormInputs(form)
+				let postData = Object.assign(defaults, userInputs)
+
+				// post-process values
+				if (postData.MobilePhone) {
+					postData.MobilePhone = postData.MobilePhone.replace(/^0{1}/, '')
+				}
+
+				fetch(getPostURL(), {
+					method: 'POST',
+					body: Object.keys(postData).reduce((formData, k) => {
+						formData.append(k, postData[k]);
+						return formData;
+					}, new FormData())
+				})
+				.then(response => response.json())
+				.then(response => {
+					console.log('response', response)
+
+					hideFullPageLoading()
+					changeToPage(2)
+
+					if (response.Supporter) { // ok, go to next page
+						sendPetitionTracking("2020-ocean_sanctuaries_tao")
+					}
+				})
+				.catch(error => {
+					hideFullPageLoading()	// something wrong
+					alert(error)
+					console.error(error)
+				})
+
+			} catch (e) {
+				console.error(e)
+			}
+
+			return false
+		},
+		invalidHandler: function(event, validator) {
+			// 'this' refers to the form
+			var errors = validator.numberOfInvalids();
+			if (errors) {
+				// console.log(errors)
+				var message = errors===1
+					? 'You missed 1 field. It has been highlighted'
+					: 'You missed ' + errors + ' fields. They have been highlighted';
+				$("div.error").show();
+			} else {
+				$("div.error").hide();
+			}
+		}
+	});
+
+})
+
+// activate the share block
+$(() => {
+	var el = document.querySelector(".en-share-block"),
+		url = el.getAttribute("data-url"),
+		text = el.getAttribute("data-text")
+
+	if ( !url) {
+		console.log("Cannot find the url. Exit share_block")
+	}
+
+	var mainButton = el.querySelector(".main-share-button")
+	var shareUrls = {}
+	shareUrls.fb = "https://www.facebook.com/sharer/sharer.php?u="+encodeURIComponent(url)+"&t="+encodeURIComponent(text)
+	shareUrls.line = "https://line.naver.jp/R/msg/text/?"
+		+ encodeURIComponent(text) + "%0D%0A"
+		+ encodeURIComponent(url)
+
+	if (navigator.share) {
+		function handleClickMainShare(e) {
+			navigator.share({
+			  text: text,
+			  url: url
+			})
+		}
+
+		mainButton.addEventListener('click', handleClickMainShare);
+		mainButton.addEventListener('touchstart', handleClickMainShare);
+	} else {
+		mainButton.href = shareUrls.fb
+	}
+
+	// setup urls for buttons
+	for (var name in shareUrls) {
+		var theBtn = el.querySelector("."+name)
+		if (theBtn) {
+			theBtn.href = shareUrls[name]
+		}
+	}
+
+})
+
+// animate the progress bar
+$(() => {
+	let {numSignupTarget, numSignup} = getNumSignupsAndTarget()
+
+	$(".signup-target").text(numSignupTarget.toLocaleString())
+	$(".signup-current").text(numSignup.toLocaleString())
+
+	let animateMilliSec = 1200
+	let $signupCurrEl = $(".signup-current")
+
+  $signupCurrEl.prop('Counter',0).animate({
+		Counter: numSignup
+  }, {
+    duration: animateMilliSec,
+    easing: 'swing',
+    step: function (now) {
+			$signupCurrEl.text(Math.ceil(now).toLocaleString());
+    }
+  });
+
+	$(".meter > span").each(function() {
+		$(this)
+			.data("origWidth", $(this).width())
+			.width(0)
+			.animate({
+				width: ""+Math.ceil(numSignup/numSignupTarget*100)+"%"
+			}, animateMilliSec);
+	});
+})
+
+// handle page changes
+const changeToPage = (pageNo) => {
+	console.log('changeToPage', pageNo)
+	if (pageNo===1) {
+		$("#page-2").hide()
+	} else if (pageNo==2) {
+		$("#page-1").hide()
+		$("#page-2").show()
+
+		// scroll to the succ element
+		if ($(window).width() <= 800) {
+			let enc = document.querySelector("#page-2")
+			let top = enc.getBoundingClientRect().top+window.pageYOffset
+			window.scrollTo(0, top);
+		}
+
+		document.querySelector(".floating-signup-div").remove()
+
+	} else {
+		throw new Error("Unknow pageNo: "+pageNo)
+	}
+}
+
+// init
+$(() => {
+	changeToPage(1)
 })
